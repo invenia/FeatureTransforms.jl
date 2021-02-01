bfce7f56813949e9384031105e6ecb6f217570f0
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-02-01 18:51:03 +0000

    Test both sin and cos

--- test/periodic.jl
+++ test/periodic.jl
@@ -1,20 +1,23 @@
 @testset "periodic" begin
     period = 5
     phase_shift = 2
-    p = Periodic(sin, period, phase_shift)
 
-    @test p isa Transform
+    @testset for periodic_function in [cos, sin]
+        p = Periodic(periodic_function, period, phase_shift)
 
-    @testset "Vector" begin
-        x = [1, 2, 3, 4, 5]
-        expected = sin.((2π / period) .* x .+ phase_shift)
+        @test p isa Transform
 
-        @test Transforms.apply(x, p) == expected
-        @test p(x) == expected
+        @testset "Vector" begin
+            x = [1., 2., 3., 4., 5.]
+            expected = periodic_function.((2π / period) .* x .+ phase_shift)
 
-        _x = copy(x)
-        Transforms.apply!(_x, p)
-        @test _x == expected
+            @test Transforms.apply(x, p) == expected
+            @test p(x) == expected
+
+            _x = copy(x)
+            Transforms.apply!(_x, p)
+            @test _x == expected
+        end
     end
 
 end

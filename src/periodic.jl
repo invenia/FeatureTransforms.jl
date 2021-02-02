7cdb2b752e75b2eb3cc2d1e8590c4339f3ef88c8
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-02-02 13:50:43 +0000

    Allow both Real and TimeType field types

--- src/periodic.jl
+++ src/periodic.jl
@@ -1,14 +1,21 @@
 const PeriodicFunction = Union{typeof(cos), typeof(sin)}
+const PeriodicInput = Union{Real,TimeType}
 
 """
     Periodic(f, period, phase_shift) <: Transform
 
-Applies a periodic function `f` with the given period and phase shift to the data.
+Applies a periodic function `f` with provided `period` and `phase_shift` to the data.
+
+# Fields
+* `f::PeriodicFunction`: the periodic function (either `cos` or `sin`)
+* `period<:PeriodicInput`: the duration it takes for the periodic function to repeat
+* `phase_shift<:PeriodicInput`: adjusts the phase of the periodic function. A positive
+    value translates the function to the left, toward lower input values.
 """
-struct Periodic <: Transform
+struct Periodic{T<:PeriodicInput} <: Transform
     f::PeriodicFunction
-    period::Real
-    phase_shift::Real
+    period::T
+    phase_shift::T
 end
 
 function _apply!(x::AbstractArray{T}, P::Periodic; kwargs...) where T <: Real

5528c0bb4801dcc0f81148ef7fc013dac6f0c5dc
Nicole Epp <nicole.epp@invenia.ca>
2021-02-02 17:10:51 -0600

    Implement cols kwarg

--- src/linear_combination.jl
+++ src/linear_combination.jl
@@ -18,28 +18,42 @@ end
 
 _sum_row(row, col_weights) = sum(map(*, row, col_weights))
 
-function apply(x::AbstractVector, LC::LinearCombination; kwargs...)
+"""
+    apply(x, LC::LinearCombination; cols=Colon())
+
+Applies the [`LinearCombination`](@ref) to each of the specified columns in `x`.
+If no `cols` are specified, then the [`LinearCombination`](@ref) is applied to all columns.
+"""
+function apply(x::AbstractVector, LC::LinearCombination; cols=Colon())
     # Treat each element as it's own column
     # Error if dimensions don't match
-    num_cols = length(x)
+    num_cols = cols isa Colon ? length(x) : length(cols)
     _check_dimensions_match(LC, num_cols)
 
-    return [_sum_row(x, LC.col_weights)]
+    return [_sum_row(x[cols], LC.col_weights)]
 end
 
-function apply(x::AbstractArray, LC::LinearCombination; kwargs...)
+function apply(x::AbstractArray, LC::LinearCombination; cols=Colon())
     # Error if dimensions don't match
-    num_cols = size(x, 2)
+    num_cols = cols isa Colon ? size(x, 2) : length(cols)
     _check_dimensions_match(LC, num_cols)
 
-    return [_sum_row(row, LC.col_weights) for row in eachrow(x)]
+    return [_sum_row(row[cols], LC.col_weights) for row in eachrow(x)]
 end
 
-function apply(x, LC::LinearCombination; kwargs...)
+function apply(x, LC::LinearCombination; cols=nothing)
     # Error if dimensions don't match
-    column_names = Tables.columnnames(x)
-    num_cols = length(column_names)
+    num_cols = cols === nothing ? length(Tables.columnnames(x)) : length(cols)
     _check_dimensions_match(LC, num_cols)
 
-    return [_sum_row(row, LC.col_weights) for row in Tables.rows(x)]
+    # Keep the generic form when not specifying column names
+    # because that is much more performant than selecting each col by name
+    if cols === nothing
+        return [_sum_row(row, LC.col_weights) for row in Tables.rows(x)]
+    else
+        return [
+            _sum_row([row[cname] for cname in cols], LC.col_weights)
+            for row in Tables.rows(x)
+        ]
+    end
 end
--- test/linear_combination.jl
+++ test/linear_combination.jl
@@ -14,6 +14,15 @@
             x = [1, 2, 3]
             @test_throws DimensionMismatch Transforms.apply(x, lc)
         end
+
+        @testset "specified cols" begin
+            x = [1, 2, 3]
+            cols = [2, 3]
+            expected = [-1]
+
+            @test Transforms.apply(x, lc; cols=cols) == expected
+            @test lc(x; cols=cols) == expected
+        end
     end
 
     @testset "Matrix" begin
@@ -24,14 +33,23 @@
         @test lc(M) == expected
 
         # Does this just test dims is ignored?
-        @testset "dims = $d" for d in (Colon(), 1, 2)
-            @test Transforms.apply(M, lc; dims=d) == expected
-            @test lc(M; dims=d) == expected
-        end
+        # @testset "dims = $d" for d in (Colon(), 1, 2)
+        #     @test Transforms.apply(M, lc; dims=d) == expected
+        #     @test lc(M; dims=d) == expected
+        # end
 
         @testset "dimension mismatch" begin
-           M = [1 1 1; 2 2 2]
-           @test_throws DimensionMismatch Transforms.apply(M, lc)
+            M = [1 1 1; 2 2 2]
+            @test_throws DimensionMismatch Transforms.apply(M, lc)
+        end
+
+        @testset "specified cols" begin
+            M = [1 1 5; 2 2 4]
+            cols = [2, 3]
+            expected = [-4, -2]
+
+            @test Transforms.apply(M, lc; cols=cols) == expected
+            @test lc(M; cols=cols) == expected
         end
     end
 
@@ -49,20 +67,38 @@
             nt = (a = [1, 2, 3], b = [4, 5, 6], c = [1, 1, 1])
             @test_throws DimensionMismatch Transforms.apply(nt, lc)
         end
+
+        @testset "specified cols" begin
+            nt = (a = [1, 2, 3], b = [4, 5, 6], c = [1, 1, 1])
+            cols = [:a, :b]
+            expected = [-3, -3, -3]
+
+            @test Transforms.apply(nt, lc; cols=cols) == expected
+            @test lc(nt; cols=cols) == expected
+        end
     end
 
     @testset "AxisArray" begin
         A = AxisArray([1 2; 4 5], foo=["a", "b"], bar=["x", "y"])
         expected = [-1, -1]
 
-        @testset "dims = $d" for d in (Colon(), 1, 2)
-            transformed = Transforms.apply(A, lc; dims=d)
-            @test transformed == expected
-        end
+        # @testset "dims = $d" for d in (Colon(), 1, 2)
+        #     transformed = Transforms.apply(A, lc; dims=d)
+        #     @test transformed == expected
+        # end
 
         @testset "dimension mismatch" begin
             A = AxisArray([1 2 3; 4 5 5], foo=["a", "b"], bar=["x", "y", "z"])
             @test_throws DimensionMismatch Transforms.apply(A, lc)
+        end
+
+        @testset "specified cols" begin
+            A = AxisArray([1 2 3; 4 5 5], foo=["a", "b"], bar=["x", "y", "z"])
+            cols = [1, 2]
+            expected = [-1, -1]
+
+            @test Transforms.apply(A, lc; cols=cols) == expected
+            @test lc(A; cols=cols) == expected
         end
     end
 
@@ -70,14 +106,23 @@
         A = KeyedArray([1 2; 4 5], foo=["a", "b"], bar=["x", "y"])
         expected = [-1, -1]
 
-        @testset "dims = $d" for d in (Colon(), :foo, :bar)
-            transformed = Transforms.apply(A, lc; dims=d)
-            @test transformed == expected
-        end
+        # @testset "dims = $d" for d in (Colon(), :foo, :bar)
+        #     transformed = Transforms.apply(A, lc; dims=d)
+        #     @test transformed == expected
+        # end
 
         @testset "dimension mismatch" begin
             A = KeyedArray([1 2 3; 4 5 6], foo=["a", "b"], bar=["x", "y", "z"])
             @test_throws DimensionMismatch Transforms.apply(A, lc)
+        end
+
+        @testset "specified cols" begin
+            A = KeyedArray([1 2 3; 4 5 5], foo=["a", "b"], bar=["x", "y", "z"])
+            cols = [1, 2]
+            expected = [-1, -1]
+
+            @test Transforms.apply(A, lc; cols=cols) == expected
+            @test lc(A; cols=cols) == expected
         end
     end
 
@@ -91,6 +136,15 @@
         @testset "dimension mismatch" begin
             df = DataFrame(:a => [1, 2, 3], :b => [4, 5, 6], :c => [1, 1, 1])
             @test_throws DimensionMismatch Transforms.apply(df, lc)
+        end
+
+        @testset "specified cols" begin
+            df = DataFrame(:a => [1, 2, 3], :b => [4, 5, 6], :c => [1, 1, 1])
+            cols = [:b, :c]
+            expected = [3, 4, 5]
+
+            @test Transforms.apply(df, lc; cols=cols) == expected
+            @test lc(df; cols=cols) == expected
         end
     end
 end

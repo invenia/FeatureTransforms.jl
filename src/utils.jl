e36b9f866dc9fcafc4c851e9fdf5e3906e50c2f6
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-02-17 19:14:39 +0000

    Use `mapslices` dims convention in `apply!`

--- src/utils.jl
+++ src/utils.jl
@@ -11,3 +11,14 @@ function _try_copy(data)
         deepcopy(data)
     end
 end
+
+function invert_dims(A::AbstractArray, dims)
+    ndims(A) == 1 && return dims
+    # TODO: support named dims
+    inverted_dims = setdiff(1:ndims(A), dims)
+    if length(inverted_dims) == 1
+        inverted_dims = inverted_dims[1]
+    end
+
+    return inverted_dims
+end

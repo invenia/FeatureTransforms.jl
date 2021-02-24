c024ecac4a542320129b45f75520a726b09739f9
Nicole Epp <nicole.epp@invenia.ca>
2021-02-24 11:14:31 -0600

    Switch to Bool for OneHotEncoding Transform

--- src/one_hot_encoding.jl
+++ src/one_hot_encoding.jl
@@ -6,16 +6,16 @@ One-hot encode the categorical value for each target element.
 Construct a n-by-p binary matrix, given a `Vector` of target data `x` (of length n) and a
 `Vector` of all unique possible values in x (of length p).
 
-The element [i, j] is `1` if the i^th target in `x` corresponds to the j^th possible
-value and `0` otherwise.
+The element [i, j] is `true` if the i^th target in `x` corresponds to the j^th possible
+value and `false` otherwise.
 
 Note that this Transform does not support specifying dims other than `:` (all dims) because
 it is a one-to-many transform (for example a `Vector` input produces a `Matrix` output).
 """
-struct OneHotEncoding <: Transform
-    categories::Dict{Any, Int}
+struct OneHotEncoding{T} <: Transform
+    categories::Dict{T, Int}
 
-    function OneHotEncoding(possible_values::AbstractVector)
+    function OneHotEncoding(possible_values::AbstractVector{T}) where T
         if length(unique(possible_values)) < length(possible_values)
             throw(ArgumentError("Expected a list of all unique possible values"))
         end
@@ -23,18 +23,17 @@ struct OneHotEncoding <: Transform
         # Create a dictionary that maps unique values in the input array to column positions
         # in the sparse matrix that results from applying the OneHotEncoding transform
         categories = Dict(value => i for (i, value) in enumerate(possible_values))
-        return new(categories)
+        return new{T}(categories)
     end
 end
 
 function _apply(x, encoding::OneHotEncoding; kwargs...)
     n_categories = length(encoding.categories)
+    results = zeros(Bool, length(x), n_categories)
 
-    results = zeros(Int, length(x), n_categories)
-
-    @views for (i, value) in enumerate(x)
+    for (i, value) in enumerate(x)
         col_pos = encoding.categories[value]
-        results[i, col_pos] = 1
+        results[i, col_pos] = true
     end
 
     return results

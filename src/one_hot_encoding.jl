ac2f78ece3f1073caf447ab0724d86e1b2c0f496
Nicole Epp <nicole.epp@invenia.ca>
2021-02-24 14:32:36 -0600

    Make OneHotEncoding return type flexible

--- src/one_hot_encoding.jl
+++ src/one_hot_encoding.jl
@@ -1,5 +1,5 @@
 """
-    OneHotEncoding <: Transform
+    OneHotEncoding{R<:Real} <: Transform
 
 One-hot encode the categorical value for each target element.
 
@@ -7,15 +7,16 @@ Construct a n-by-p binary matrix, given a `Vector` of target data `x` (of length
 `Vector` of all unique possible values in x (of length p).
 
 The element [i, j] is `true` if the i^th target in `x` corresponds to the j^th possible
-value and `false` otherwise.
+value and `false` otherwise. Note that `R`can be specified to determine the return type
+of results. It defaults to a `Matrix` of `Bool`s.
 
 Note that this Transform does not support specifying dims other than `:` (all dims) because
 it is a one-to-many transform (for example a `Vector` input produces a `Matrix` output).
 """
-struct OneHotEncoding{T} <: Transform
+struct OneHotEncoding{R<:Real, T} <: Transform
     categories::Dict{T, Int}
 
-    function OneHotEncoding(possible_values::AbstractVector{T}) where T
+    function OneHotEncoding{R}(possible_values::AbstractVector{T}) where {R<:Real, T}
         if length(unique(possible_values)) < length(possible_values)
             throw(ArgumentError("Expected a list of all unique possible values"))
         end
@@ -23,13 +24,17 @@ struct OneHotEncoding{T} <: Transform
         # Create a dictionary that maps unique values in the input array to column positions
         # in the sparse matrix that results from applying the OneHotEncoding transform
         categories = Dict(value => i for (i, value) in enumerate(possible_values))
-        return new{T}(categories)
+        return new{R, T}(categories)
     end
 end
 
-function _apply(x, encoding::OneHotEncoding; kwargs...)
+function OneHotEncoding(possible_values::AbstractVector{T}) where T
+    return OneHotEncoding{Bool}(possible_values)
+end
+
+function _apply(x, encoding::OneHotEncoding{R}; kwargs...) where R <: Real
     n_categories = length(encoding.categories)
-    results = zeros(Bool, length(x), n_categories)
+    results = zeros(R, length(x), n_categories)
 
     for (i, value) in enumerate(x)
         col_pos = encoding.categories[value]

da2184105f7de9404b3bdfa486cf8256b289fd49
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-04-16 17:36:07 +0100

    Add note to LC docstring

--- src/linear_combination.jl
+++ src/linear_combination.jl
@@ -1,12 +1,17 @@
 """
     LinearCombination(coefficients) <: Transform
 
-Calculates the linear combination of a collection of terms weighted be some `coefficients`.
+Calculates the linear combination of a collection of terms weighted by some `coefficients`.
 
 When applied to an N-dimensional array, `LinearCombination` reduces along the `dim` provided
 and returns an (N-1)-dimensional array.
 
 If no `inds` are specified, then the transform is applied to all elements.
+
+!!!note
+    The current default is that `dims=1` but this behaviour will be deprecated in a future
+    release and the `dims` keyword argument will have to be specified explicitly.
+    https://github.com/invenia/FeatureTransforms.jl/issues/82
 """
 struct LinearCombination <: Transform
     coefficients::Vector{Real}

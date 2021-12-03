18f65927b4e190d14f77390661e8e3264e52cbcb
Alex Robson <alex.robson@invenialabs.co.uk>
2021-12-03 12:28:42 +0000

    Add docstring

--- src/log.jl
+++ src/log.jl
@@ -1,4 +1,10 @@
+"""
+    LogTransform <: Transform
 
+Logarithmically transform the data through: sign(x) * log(|x| + 1).
+
+This allows positive domains elements to be transformed. 
+"""
 struct LogTransform <: Transform end
 cardinality(::LogTransform) = OneToOne()
 
@@ -10,13 +16,18 @@ function _apply(A::AbstractArray, transform::LogTransform; inverse=false, kwargs
     return _logtransform.(A)
 end
 
-# Hyperbolic
+
+"""
+    InverseHyperbolicSine <: Transform
+
+Logarithmically transform the data through: log(x + √(x² + 1)).
+
+This is the inverse hyperbolic sine. 
+"""
 struct InverseHyperbolicSine <: Transform end
 cardinality(::InverseHyperbolicSine) = OneToOne()
 
 function _apply(A::AbstractArray, transform::InverseHyperbolicSine; inverse=false, kwargs...)
     inverse && return sinh.(A)
     return asinh.(A)
-end
-
-
+end      
